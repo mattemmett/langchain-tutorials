1539c8f9d35f9f24500181e48dccde6c97320947
Matt Emmett <matt@mattemmett.com>
2024-12-03 14:53:30 +0000

    Completed Build a Simple LLM Application tutorial

--- simple-llm-app/llm-app.py
+++ simple-llm-app/llm-app.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 from langchain_openai import AzureChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
@@ -20,11 +21,41 @@ model = AzureChatOpenAI(
     openai_api_version="2023-05-15",
 )
 
-messages = [
-        SystemMessage(content="Translate the following from English into Italian"),
-        HumanMessage(content="hi!"),
+# Simple chat
+#messages = [
+#        SystemMessage(content="Translate the following from English into Italian"),
+#        HumanMessage(content="hi!"),
+#    ]
+
+#response = model.invoke(messages)
+ 
+#print(response)
+
+# Streaming chat
+
+#messages = [
+#        SystemMessage(content="Translate the following from English into Italian"),
+#        HumanMessage(content="hi!"),
+#    ]
+
+#for token in model.stream(messages):
+#    print(token.content, end="|")
+
+# Chat with prompt template
+
+system_template = "Translate the following from English in {language}"
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_template),
+        ("user", "{text}"),
     ]
+)
 
-response = model.invoke(messages)
+prompt = prompt_template.invoke({"language": "Italian", "text": "hi!"})
 
-print(response)
+
+print(prompt.to_messages())
+
+response = model.invoke(prompt)
+print(response.content)
